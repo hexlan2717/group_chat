ecd45647755b8effc7b11eea9e381e1f93461543
hexlan2717 <hexlan2717@gmail.com>
2023-02-13 09:38:24 +0600

    Initial commit Generated by create-expo-app 1.1.4.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View  className="flex-1 items-center pt-[56px]">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
